48465e023fb13ef4130216d5928906418f3456e6
MillarCD <diego12millar@gmail.com>
2022-09-12 20:42:30 -0300

    cambio en respuestas al transferir

--- mono_api/main.py
+++ mono_api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
-from ast import literal_eval
+# from ast import literal_eval
 
 from .models.manager import Manager 
 from .models.game import Game
@@ -12,8 +13,31 @@ app = FastAPI()
 manager = Manager()
 game = Game()
 
+class Transfer(BaseModel):
+    from_: str
+    to: str
+    monto: int
+
+class Create(BaseModel):
+    name: str
+
 
 # ENDPOINTS
+@app.post('/transfer')
+async def transfer(data: Transfer):
+    playersList = game.transfer(data.from_, data.to, data.monto)
+    for player in playersList:
+        await manager.sendMsg({'content': 'transfer', 'name': player.name, 'cash': player.cash}, player.websocket)
+
+    return {'content': 'transfer', 'name': playersList[0].name, 'cash': playersList[0].cash}
+
+@app.post('/create')
+async def create_player(data: Create):
+    res, player = game.addPlayer( Player(data['name'], websocket ) )
+    if res:
+        await manager.broadcast({'content': 'players', 'players': game.getPlayersNames()})
+    return {'content': 'create', 'name': player.name, 'cash': player.cash, 'players': game.getPlayersNames()}
+
 
 @app.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket):
@@ -26,8 +50,6 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_json()
-            print('[DATA]: ', data)
-            print('[DATA TYPE]', type(data))
 #             data = literal_eval(data)
 
             # identificar el contenido del mensaje
